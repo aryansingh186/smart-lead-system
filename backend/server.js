22885a78a6265d701099f388d396b950cf2c8188
aryansingh186 <aryansingh700108@gmail.com>
2025-12-23 15:50:42 +0530

    cors

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(cors({
   origin: [
     'http://localhost:3000',
-    'https://smart-lead-system-five.vercel.app'
+    'https://smart-lead-system-qgya.vercel.app/'
   ],
   credentials: true,
   methods: ['GET','POST','PUT','DELETE','OPTIONS']
